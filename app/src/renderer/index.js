51b5a4c37ff9829523465c96484a4e0bf9a99717
Banu Calin <banucaliniulian@gmail.com>
2023-01-10 21:03:51 +0200

    Tauri base HTML/CSS and JS app

--- app/src/renderer/index.js
+++
@@ -1,3 +0,0 @@
-console.log(versions.node());
-console.log(versions.chrome());
-console.log(versions.electron());
--- setup.sh
+++ setup.sh
@@ -1,17 +1,1 @@
 #!/bin/bash
-
-# adduser node audio
-# adduser node video
-
-cd app
-
-npm install
-npx electron-rebuild
-
-sudo chown root node_modules/electron/dist/chrome-sandbox
-sudo chmod 4755 node_modules/electron/dist/chrome-sandbox
-
-# if [ ! -d "/var/run/dbus" ]; then
-#     sudo mkdir -p /var/run/dbus 
-#     sudo dbus-daemon --config-file=/usr/share/dbus-1/system.conf --print-address
-# fi
