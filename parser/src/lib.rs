191989741d74a22f11b6d6738e5763f018986c14
Banu Calin <banucaliniulian@gmail.com>
2023-01-12 16:39:54 +0200

    Clean parser code

--- parser/src/lib.rs
+++ parser/src/lib.rs
@@ -1,19 +1,3 @@
-mod utils;
-
-use wasm_bindgen::prelude::*;
-
-// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
-// allocator.
-#[cfg(feature = "wee_alloc")]
-#[global_allocator]
-static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
-
-static mut TEST: u32 = 0;
-
-#[wasm_bindgen]
-pub fn increment() -> u32 {
-    unsafe {
-        TEST += 1;
-        TEST
-    }
+pub fn sum(a: u32, b: u32) -> u32 {
+    a + b
 }
