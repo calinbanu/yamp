02e910dc1d6adfaa7629428dc76a494508b91e9a
Banu Calin <banucaliniulian@gmail.com>
2023-03-29 22:58:33 +0300

    Remove tauri framework and update env

--- build-wasm-parser.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-
-cd $SCRIPTPATH/parser
-rm -rf ./app/parser-wasm
-
-wasm-pack build --target bundler --out-dir ../app/parser-wasm --out-name parser-wasm .
